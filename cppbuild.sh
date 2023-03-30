7cbc2dc66f809cc22e5fd2636a1eb3729fa43fcb
Kennan Hunter <kennanhunter5@gmail.com>
2023-03-30 10:18:54 -0400

    change build config to only compile depthai

--- cppbuild.sh
+++ cppbuild.sh
@@ -14,37 +14,37 @@ ARCH=(`uname -m | tr '[A-Z]' '[a-z]'`)
 case $KERNEL in
     darwin)
         OS=macosx
-        ;;
+    ;;
     mingw32*)
         OS=windows
         KERNEL=windows
         ARCH=x86
-        ;;
+    ;;
     mingw64*)
         OS=windows
         KERNEL=windows
         ARCH=x86_64
-        ;;
+    ;;
     *)
         OS=$KERNEL
-        ;;
+    ;;
 esac
 case $ARCH in
     arm64)
         ARCH=arm64
-        ;;
+    ;;
     arm*)
         ARCH=arm
-        ;;
+    ;;
     aarch64*)
         ARCH=arm64
-        ;;
+    ;;
     i386|i486|i586|i686)
         ARCH=x86
-        ;;
+    ;;
     amd64|x86-64)
         ARCH=x86_64
-        ;;
+    ;;
 esac
 PLATFORM=$OS-$ARCH
 EXTENSION=
@@ -54,27 +54,27 @@ while [[ $# > 0 ]]; do
     case "$1" in
         -platform=*)
             PLATFORM="${1#-platform=}"
-            ;;
+        ;;
         -platform)
             shift
             PLATFORM="$1"
-            ;;
+        ;;
         -extension=*)
             EXTENSION="${1#-extension=}"
-            ;;
+        ;;
         -extension)
             shift
             EXTENSION="$1"
-            ;;
+        ;;
         install)
             OPERATION=install
-            ;;
+        ;;
         clean)
             OPERATION=clean
-            ;;
+        ;;
         *)
             PROJECTS+=("$1")
-            ;;
+        ;;
     esac
     shift
 done
@@ -104,19 +104,19 @@ case $PLATFORM in
     android-arm)
         export ANDROID_FLAGS="-DANDROID -fPIC -ffunction-sections -funwind-tables -fstack-protector-strong -target armv7a-linux-android24 -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 -z text -Wno-unused-command-line-argument -Wno-unknown-warning-option -Wno-ignored-optimization-argument -Wl,--fix-cortex-a8 -Wl,--no-undefined -nostdlib++"
         export ANDROID_LIBS="-llog -lc++_static -lc++abi -landroid_support -ldl -lm -lc"
-        ;;
+    ;;
     android-arm64)
         export ANDROID_FLAGS="-DANDROID -fPIC -ffunction-sections -funwind-tables -fstack-protector-strong -target aarch64-linux-android24 -march=armv8-a -z text -Wno-unused-command-line-argument -Wno-unknown-warning-option -Wno-ignored-optimization-argument -Wl,--no-undefined -nostdlib++"
         export ANDROID_LIBS="-llog -lc++_static -lc++abi -ldl -lm -lc"
-        ;;
+    ;;
     android-x86)
         export ANDROID_FLAGS="-DANDROID -fPIC -ffunction-sections -funwind-tables -fstack-protector-strong -target i686-linux-android24 -march=i686 -mtune=atom -mssse3 -mfpmath=sse -z text -Wno-unused-command-line-argument -Wno-unknown-warning-option -Wno-ignored-optimization-argument -Wl,--no-undefined -nostdlib++"
         export ANDROID_LIBS="-llog -lc++_static -lc++abi -landroid_support -ldl -lm -lc"
-        ;;
+    ;;
     android-x86_64)
         export ANDROID_FLAGS="-DANDROID -fPIC -ffunction-sections -funwind-tables -fstack-protector-strong -target x86_64-linux-android24 -march=x86-64 -mtune=atom -z text -Wno-unused-command-line-argument -Wno-unknown-warning-option -Wno-ignored-optimization-argument -Wl,--no-undefined -nostdlib++"
         export ANDROID_LIBS="-llog -lc++_static -lc++abi -ldl -lm -lc"
-        ;;
+    ;;
 esac
 
 TOP_PATH=`pwd`
@@ -129,21 +129,21 @@ function download {
         DOWNLOADSTATUS=$?
         if [ "$DOWNLOADSTATUS" -eq 28 ]
         then
-		echo "Download timed out, waiting 5 minutes then trying again"
-		rm "$TOP_PATH/downloads/$2"
-		sleep 600
-        	curl -L "$1" -o "$TOP_PATH/downloads/$2" --fail
-        	if [ $? -ne 0 ]
-        	then
-			echo "File still could not be downloaded!"
-			rm "$TOP_PATH/downloads/$2"
-			exit 1
-    		fi
+            echo "Download timed out, waiting 5 minutes then trying again"
+            rm "$TOP_PATH/downloads/$2"
+            sleep 600
+            curl -L "$1" -o "$TOP_PATH/downloads/$2" --fail
+            if [ $? -ne 0 ]
+            then
+                echo "File still could not be downloaded!"
+                rm "$TOP_PATH/downloads/$2"
+                exit 1
+            fi
         elif [ "$DOWNLOADSTATUS" -ne 0 ]
         then
-		echo "File could not be downloaded!"
-		rm "$TOP_PATH/downloads/$2"
-		exit 1
+            echo "File could not be downloaded!"
+            rm "$TOP_PATH/downloads/$2"
+            exit 1
         fi
     fi
     ln -sf "$TOP_PATH/downloads/$2" "$2"
@@ -158,7 +158,7 @@ function sedinplace {
 }
 
 if [[ -z ${PROJECTS:-} ]]; then
-    PROJECTS=(opencv ffmpeg flycapture spinnaker libdc1394 libfreenect libfreenect2 librealsense librealsense2 videoinput artoolkitplus chilitags flandmark arrow hdf5 hyperscan lz4 mkl mkl-dnn dnnl openblas arpack-ng cminpack fftw gsl cpython numpy scipy gym llvm libpostal libraw leptonica tesseract caffe openpose cuda nvcodec opencl mxnet pytorch tensorflow tensorflow-lite tensorrt tritonserver depthai ale onnx ngraph onnxruntime tvm bullet liquidfun qt skia cpu_features modsecurity systems)
+    PROJECTS=(depthai)
 fi
 
 for PROJECT in ${PROJECTS[@]}; do
@@ -173,10 +173,10 @@ for PROJECT in ${PROJECTS[@]}; do
                 source ../cppbuild.sh
                 popd
             fi
-            ;;
+        ;;
         clean)
             echo "Cleaning \"$PROJECT\""
             rm -Rf $PROJECT/cppbuild
-            ;;
+        ;;
     esac
 done
